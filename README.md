a6b7222bf26785e78fb5ce5227748d526a247f67
Mark Pimperl <mark.pimperl@pc.gov.au>
2023-12-12 18:20:48 +1100

    Multiple tabviz/data table rendered/styling

--- README.md
+++ README.md
@@ -12,33 +12,33 @@
 - **[Wishlist/Future features](#wishlist-toc)**
   
 
-## App [<sup>[TOC]</sup>](#table-of-contents)
+## App [<sup><sup><sup>[TOC]</sup></sup></sup>](#table-of-contents)
 
 The App is [Tableau visualisations: Dashboard](https://infoshape.github.io/jsd-final-project/) [Under development].
 
-The diagnostic page to develop code is [Tableau JavaScript API v3 testing](https://infoshape.github.io/jsd-final-project/diags.html).
+The diagnostic page used to develop the App is [Tableau JavaScript API v3 testing](https://infoshape.github.io/jsd-final-project/diags.html).
 
-## Overview [<sup>[TOC]</sup>](#table-of-contents)
+## Overview [<sup><sup><sup>[TOC]</sup></sup></sup>](#table-of-contents)
 
 Tableau JavaScript API v3 to embed multiple visualisations into one webpage with a common set of HTML filter buttons to control them via the API.  Also use the API to extract the underlying data of vizzes and pivot the data to produce an accessible data table alternative. 
 
-## Planning stage [<sup>[TOC]</sup>](#table-of-contents)
+## Planning stage [<sup><sup><sup>[TOC]</sup></sup></sup>](#table-of-contents)
 
 *Coming...*
 
-## Screenshots [<sup>[TOC]</sup>](#table-of-contents)
+## Screenshots [<sup><sup><sup>[TOC]</sup></sup></sup>](#table-of-contents)
 
 *Coming...*
 
-## Technical hurdle [<sup>[TOC]</sup>](#table-of-contents)
+## Technical hurdle [<sup><sup><sup>[TOC]</sup></sup></sup>](#table-of-contents)
 
 *Coming...*
 
-## What I learned [<sup>[TOC]</sup>](#table-of-contents)
+## What I learned [<sup><sup><sup>[TOC]</sup></sup></sup>](#table-of-contents)
 
 *Coming...*
 
-## Other technology [<sup>[TOC]</sup>](#table-of-contents)
+## Other technology [<sup><sup><sup>[TOC]</sup></sup></sup>](#table-of-contents)
 
 | Technology                    | Description                                              |
 | -------------------------- | -------------------------------------------------------- |
@@ -47,6 +47,12 @@ Tableau JavaScript API v3 to embed multiple visualisations into one webpage with
 | `Bootstrap 5`                       | Frontend framework/ <br />dashboard template                               |
 
 
-## Wishlist/Future features [<sup>[TOC]</sup>](#table-of-contents)
+## Wishlist/Future features [<sup><sup><sup>[TOC]</sup></sup></sup>](#table-of-contents)
 
 *Coming...*
+
+<style>
+sup {
+  top: -0.25em !important;
+}
+</style>
--- js/main.js
+++ js/main.js
@@ -3,83 +3,147 @@ import {
   TableauEventType,
 } from "https://public.tableau.com/javascripts/api/tableau.embedding.3.latest.js";
 
-export const vizUrls = [
-  //"https://public.tableau.com/views/2023_CtG16_CultureLanguage_Supp/FigureSE16e_2",
+//"https://public.tableau.com/views/2023_CtG16_CultureLanguage_Supp/FigureSE16e_2",
+//"https://public.tableau.com/views/2023_CtG06_EducationPathways_Supp/FigureSE6a_1",
+//"https://public.tableau.com/views/2023_CtG06_EducationPathways_Supp/FigureSE6a_2",
+//"https://public.tableau.com/views/2023_CtG06_EducationPathways_Supp/FigureSE6a_4"
+
+let vizWorksheets= [];
+const vizUrls = [
+  "https://public.tableau.com/views/2023_CtG06_EducationPathways_Supp/FigureSE6c_1",
   "https://public.tableau.com/views/CtG04_EarlyYears_Disaggs_2023/FigureCtG4_5",
-  "https://public.tableau.com/views/2023_CtG06_EducationPathways_Supp/FigureSE6a_1",
-  "https://public.tableau.com/views/2023_CtG06_EducationPathways_Supp/FigureSE6a_2",
-  "https://public.tableau.com/views/2023_CtG06_EducationPathways_Supp/FigureSE6a_3"
+  "https://public.tableau.com/views/2023_CtG06_EducationPathways_Supp/FigureSE6a_3",
 ] // vizzes
 
-export function initialiseViz( vizIndex, vizUrl ) {
+for( let i=0; i < vizUrls.length; i++ ){
+  initialiseViz(i, vizUrls[i]);
+}; // initialise vizzes
+
+function initialiseViz( vizIndex, vizUrl ) {
+
   const viz = new TableauViz();
   viz.hideTabs = true;
 
   const onFirstInteractive = () => {
-    getTableData( viz, 'default' );
+    const currentWorksheet = viz.workbook.activeSheet.worksheets[0];
+    vizWorksheets[vizIndex] = currentWorksheet;
+    console.log('viz worksheets:', vizWorksheets);
+    getTableData( vizIndex, 'default' );
   };
+
   viz.addEventListener(TableauEventType.FirstInteractive, onFirstInteractive);
+  viz.addEventListener(TableauEventType.FilterChanged, onFilterChanged);
   viz.src = vizUrl;
   document.getElementById(`tableauViz${vizIndex}`).appendChild(viz);
+
 } // initialiseViz()
 
-export async function getTableData( viz, htmlTable = 'current' ){
-  let simpleDataTable = [];
-  const currentWorksheet = viz.workbook.activeSheet.worksheets[0];
-  const dataHeader = await currentWorksheet.getSummaryColumnsInfoAsync();
-  const dataLabelIndexes = processHeaders( dataHeader );
+export async function getTableData( vizIndex, htmlTable = 'current' ){
+
+  const currentWorksheet = vizWorksheets[ vizIndex ];
+  console.log('vizIndex:', vizIndex);
+  console.log('currentWorksheet:', currentWorksheet);
+  const dataLabelIndexes = await requestHeaderProcessColumns( currentWorksheet );
+  const dataTable = await requestData( currentWorksheet ); 
+  console.log('Data table:', dataTable);
+
+  // Set table-wide values
+  const tableNumber = dataTable[0][dataLabelIndexes.TableNumber].formattedValue;
+  const unit = dataTable[0][dataLabelIndexes.Unit].formattedValue;
+  console.log('Val:', dataLabelIndexes.DataValue);
+  console.log('1dp:', dataLabelIndexes.Value1dp);
+  
+  const simplifiedDataTable = reduceData( dataTable, dataLabelIndexes );
+  const pivotTable = pivotData( simplifiedDataTable );
+  console.log('Pivot table:', pivotTable);
+  renderHtml( pivotTable, tableNumber, unit, vizIndex );
+
+}; // getTableData()
+
+function onFilterChanged(){
+  alert('Filter changed!');
+}; // onFilterChanged()
+
+async function requestHeaderProcessColumns( worksheet ){
+
+  const dataHeader = await worksheet.getSummaryColumnsInfoAsync();
+
+  // Data column names have been prefixed: 'Grp'(optional), 'Row', 'Col' to identify inputs to the pivot.
+  // The data value '1dp' (1 decimal place) has precedence over 'Data Value' when both are present.
+  let dataLabelIndexes = {}; 
+  const labels = ["Grp", "Row", "Col", "Data String", "Unit", "Table Number", "(Data Value)", "Value (1dp)"];
+  const dataLabels  = dataHeader.map( h => h.fieldName );
+
+  for( let i=0; i < labels.length; i++ ){ // Find the array indexes of all the important columns.
+    
+    const currentDataLabel = dataLabels.findIndex( l => l.includes( labels[i] ) );
+    if( currentDataLabel > -1 ){
+      dataLabelIndexes[ labels[i].replace(/[\s\(\)]/g, '') ] = currentDataLabel;  // regex removes spaces and brackets
+    }
+  }
   console.log('Data labels:', dataLabelIndexes);
+  return dataLabelIndexes; 
+
+}; // requestHeaderProcessColumns()
+
+async function requestData( worksheet ){
+
   const summaryDataOptions = {
       maxRows: 0,
       ignoreAliases: false,
       ignoreSelection: true,
       includeDataValuesOption: 'only-formatted-values',
   };
-  const summaryData = await currentWorksheet.getSummaryDataAsync(summaryDataOptions);
-  const dataTable = summaryData.data;
-  console.log('Data table:', dataTable);
 
-  const tableNumber = dataTable[0][dataLabelIndexes.TableNumber].formattedValue;
-  const unit = dataTable[0][dataLabelIndexes.Unit].formattedValue;
-  console.log('Val:', dataLabelIndexes.DataValue);
-  console.log('1dp:', dataLabelIndexes.Value1dp);
+  const summaryData = await worksheet.getSummaryDataAsync(summaryDataOptions);
+  return summaryData.data;
+
+}; // requestData()
+
+function reduceData( dataTable, dataLabelIndexes ){
+
+  let simplifiedDataTable = [];
   const dataIndex = ( dataLabelIndexes.Value1dp > -1  ) ? dataLabelIndexes.Value1dp : dataLabelIndexes.DataValue;
   const pivotInputOrder = [ dataLabelIndexes.Row, dataLabelIndexes.Col, dataIndex ];
-  
   console.log('Input order:', pivotInputOrder);
 
   for( let i=0; i < dataTable.length; i++ ){
-    simpleDataTable[i] = [];
+
+    simplifiedDataTable[i] = [];
     let j = 0;
     for ( const inputIndex of pivotInputOrder ) {
+
       if( dataIndex === inputIndex ){ // data value/1dp
+
         if( dataTable[i][dataIndex].formattedValue === 'Null' ){
-          simpleDataTable[i][j] = dataTable[i][dataLabelIndexes.DataString].formattedValue;
+
+          simplifiedDataTable[i][j] = dataTable[i][dataLabelIndexes.DataString].formattedValue;
+
         } else if ( typeof dataTable[i][dataIndex].formattedValue === 'number' ){
-          simpleDataTable[i][j] = dataTable[i][dataIndex].formattedValue;
+
+          simplifiedDataTable[i][j] = dataTable[i][dataIndex].formattedValue;
         } else {
-          simpleDataTable[i][j] = parseFloat(dataTable[i][dataIndex].formattedValue);
+
+          simplifiedDataTable[i][j] = parseFloat(dataTable[i][dataIndex].formattedValue);
         }
       } else { // Not a data value/1dp
-        simpleDataTable[i][j] = dataTable[i][inputIndex].formattedValue;
+
+        simplifiedDataTable[i][j] = dataTable[i][inputIndex].formattedValue;
       }
       j++;
     }
   }
-  console.log('Simple dataTable:', simpleDataTable);
-  const columnHeadings = new Set(simpleDataTable.map( (num, index, arr) => arr[index][1] ));
-  const colHeadingArray = ["", ...columnHeadings];
-  console.log('Column headings:', colHeadingArray);
-  const pivotTable = pivotData( simpleDataTable );
-  pivotTable.unshift(colHeadingArray); // Insert the header row
-  console.log('Pivot table:', pivotTable);
-  pivotToHTMLwithRowpan( pivotTable, tableNumber, unit, ["Description5"] );
+  console.log('Simple dataTable:', simplifiedDataTable);
+  return simplifiedDataTable;
 
-}; // getTableData()
+}; // reduceData()
 
-function pivotToHTMLwithRowpan( pivotTable, tableNumber, unit, rowHeaders=["Description5"] ) { // With rowspan | , datatableNum, target, rowHeaders
+function renderHtml( pivotTable, tableNumber, unit, vizIndex ) { 
+  
+  // With rowspan | , datatableNum, target, rowHeaders
   //npPresent = naPresent = napPresent = nilPresent = false;
-  const rowSpan = rows =>
+  const rowHeaders = [""]; // FIX!!!
+  const rowSpan = rows =>  // TEST PROPERLY!!!
     rows.map((row, rowIndex) =>
       row.map((item, colIndex) => ({
           data: item,
@@ -101,12 +165,16 @@ function pivotToHTMLwithRowpan( pivotTable, tableNumber, unit, rowHeaders=["Desc
     }
     return count;
   };
-  const set1dp = true;
-  const datatableNum = '1';
+
+  const set1dp = true;  // FIX!!!
+  const datatableNum = '1'; // FIX!!!
   let result = "<table data-unit='" + unit + "' class='table datatable mb-4'>";
   result += "<caption style='padding-bottom:0;color:#2c2c2c; font-weight:400;'>Data in figure " + tableNumber.substring(0, tableNumber.indexOf('.')) + '.' + datatableNum + " (" + unit + ")</caption><thead>";
+
   rowSpan(pivotTable).forEach((row, rowIndex) => {
-    if ( rowIndex === 0 ) {  // Table header
+
+    // Table header
+    if ( rowIndex === 0 ) {  
       result += "<tr>";
       row.forEach((item, colIndex) => {
           if ( colIndex < rowHeaders.length ) {
@@ -116,7 +184,9 @@ function pivotToHTMLwithRowpan( pivotTable, tableNumber, unit, rowHeaders=["Desc
           }
       });
       result += "</tr></thead><tbody>";
-    } else {  // Table body
+
+      // Table body
+    } else {  
       result += "<tr>";
       row.forEach((item, colIndex) => {
         if (item.rowSpan) {
@@ -152,6 +222,7 @@ function pivotToHTMLwithRowpan( pivotTable, tableNumber, unit, rowHeaders=["Desc
     }
   });
   result += "</tbody></table>";
+  // Table footer
   // if (npPresent || naPresent || napPresent || nilPresent) {
   //     result += "<p class='small' style='margin-top:-1.0rem;'>";
   //     if (npPresent) {
@@ -168,44 +239,33 @@ function pivotToHTMLwithRowpan( pivotTable, tableNumber, unit, rowHeaders=["Desc
   //     }
   //     result += "</p>";
   // }
-  $('#dataTable1').html(result);
-}
+  $( `#dataTable${ vizIndex }`).html( result );
+
+}; // renderHtml()
 
 function pivotData( simpleDataTable ){
 
-    const colIndex = simpleDataTable.reduce((acc, currVal) => {
-      let col = currVal[0];
-      if (!acc[col]) acc[col] = [];
-      acc[col].push(currVal);
-      return acc;
-    }, {});
-    
-    let result = [];
-    Object.keys(colIndex).map(col => {
-      let values = colIndex[col].map(arr => arr[2]);
-      result.push([col, ...values]);
-    });
+  // Prepare the header row
+  const columnHeadings = new Set(simpleDataTable.map( (num, index, arr) => arr[index][1] ));
+  const colHeadingArray = ["", ...columnHeadings]; // FIX!!!
+  console.log('Column headings:', colHeadingArray);
+
+  // Pivot the data (like crosstab) returned by the API request getSummaryDataAsync().
+  const colIndex = simpleDataTable.reduce((acc, currVal) => {
+    let col = currVal[0];
+    if (!acc[col]) acc[col] = [];
+    acc[col].push(currVal);
+    return acc;
+  }, {});
   
-    return result;
-    
-  }; // pivotData()
+  let result = [];
+  Object.keys(colIndex).map(col => {
+    let values = colIndex[col].map(arr => arr[2]);
+    result.push([col, ...values]);
+  });
 
-function processHeaders( dataHeader ){
-
-  let dataLabelIndexes = {};
-  const labels = ["Grp", "Row", "Col", "Data String", "Unit", "Table Number", "(Data Value)", "Value (1dp)"];
-  const dataLabels  = dataHeader.map( h => h.fieldName );
-
-  for( let i=0; i < labels.length; i++ ){
-    const currentDataLabel = findColumn( labels[i], dataLabels );
-    if( currentDataLabel > -1 ){
-      dataLabelIndexes[ labels[i].replace(/[\s\(\)]/g, '') ] = currentDataLabel;  // regex removes spaces and brackets
-    }
-  }
-  return dataLabelIndexes; 
-
-}; // pivotTableInputs()
-
-function findColumn( string, array ){ // returns -1 if not found
-  return array.findIndex( i => i.includes( string ) )
-}; // findColumn()
+  // Insert the header row
+  result.unshift(colHeadingArray);
+  return result;
+  
+}; // pivotData()
